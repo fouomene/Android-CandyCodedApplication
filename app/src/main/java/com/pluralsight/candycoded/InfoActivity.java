3736c05e6bc0c26086f8043e68c639bdab296e8b
fouomene <fouomenedaniel@gmail.com>
2019-11-08 20:48:17 +0100

    Start the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,7 +41,7 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 3 - Launch the Phone Activity
     // ***
     public void createPhoneIntent(View view) {
-        Uri phoneUri = Uri.parse("tel:(012)345-6789");
+        Uri phoneUri = Uri.parse("tel:0123456789");
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
         phoneIntent.setData(phoneUri);
         startActivity(phoneIntent);
